5fbf7cfe04e3b3784bf415d3366998f89206bbc7
Vince Buffalo <vsbuffalo@gmail.com>
2012-07-19 19:37:19 -0700

    early join

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # findorf: ORF prediction and annotation via blastx results of close relatives
 
+## Computational Approach
+
+The BLASTx XML format returns `iteration` XML nodes that correspond to
+each query. These *should* be in the same order as the contig
+reference FASTA file. With this, we could reduce our memory footprint
+by opening up file handles for all BLASTx files and the reference
+FASTA file and looping through them together.
+
+Currently each XML result file is being looped through to propagate
+the dictionary. This is a tad safer, a tad easier, and a bit slower.
--- findorf.py
+++ findorf.py
@@ -15,6 +15,7 @@ Specifying blastx Results
 """
 
 import sys
+import cPickle
 try:
     from Bio.Blast import NCBIXML
     from Bio.Alphabet import generic_dna, DNAAlphabet
@@ -25,13 +26,7 @@ except ImportError, e:
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description=info)
-parser.add_argument('blastx', type=str, 
-                    nargs="+", help="the output XML of BLASTx")
-parser.add_argument('--ref', type=str, required=False,
-                    help="the FASTA reference that corresponds to BLAST queries.")
-
-def parse_blastx_args(bargs):
+def parse_blastx_args(args):
     """
     Take a list of args and return a dictionary of names and files. If
     any arg is of the key:value sort, the name will be the
@@ -39,7 +34,7 @@ def parse_blastx_args(bargs):
     name.
     """
     blastx_files = dict()
-    for arg in bargs:
+    for arg in args:
         tmp = arg.split(":")
         if len(tmp) == 2:
             name, value = tmp
@@ -52,21 +47,86 @@ def parse_blastx_args(bargs):
         else:
             msg = "malformed key-value pair in argument: '%s'" % arg
             raise argparse.ArgumentTypeError(msg)
-    return blastx_files
 
+    # now open all file handles
+    try:
+        handles = dict([(k, open(f, 'r')) for k, f in blastx_files.items()])
+    except IOError, e:
+        sys.exit("could not open BLASTX XML result '%s' - no such file.\n" % e.filename)
+    return handles
+
+
+def join_blastx_results(args):
+    """
+    Each BLAST XML result file contains different alignments, each
+    with possibly multiple HSPs. The foreign key is the contig ID,
+    which is also the BLAST query ID. This of course is also the key
+    between the BLAST results and the reference contig FASTA file.
+
+    This function builds a dictionary which each key being the contig
+    ID and each value being another dictionary in which each key is
+    the BLAST result file and the values are the BLAST results.
+    """
+
+    blast_files = parse_blastx_args(args.blastx)
+
+    results = dict()
+
+    for key, blast_file in blast_files.items():
+        
+        sys.stderr.write("processing BLAST file '%s'...\t" % key)
+
+        for record in NCBIXML.parse(blast_file):
+            if not results.get(record.query, False):
+                results[record.query] = dict()
+                
+            results[record.query][key] = dict()
+            for align in record.alignments:
+                results[record.query][key][align.title] = list()
+                for hsp in align.hsps:
+                    results[record.query][key][align.title].append(hsp)
+
+        sys.stderr.write("done\n")
+        
+    return results
+
+def predict_orf():
+    """
+    
+    """
+    pass
 
 
     
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=info)
+    subparsers = parser.add_subparsers(help="sub-commands")
+
+    ## join arguments
+    parser_join = subparsers.add_parser('join', help="join each BLASTX XML results file")
+    parser_join.add_argument('blastx', type=str, 
+                             nargs="+", help="the output XML of BLASTx")
+    parser_join.add_argument('--output', type=argparse.FileType('wb'),
+                             default="joined_blastx_dbs.pkl",
+                             help="joined results of all the BLASTX results files (Python pickle file)")
+    parser_join.set_defaults(func=join_blastx_results)
+
+    ## predict arguments
+    parser_predict = subparsers.add_parser('predict', help="predict ORFs")
+    parser_predict.add_argument('--input', help="the joined results of all BLASTX files (Python pickle file; from sub-command joined)")
+    parser_predict.add_argument('--ref', type=str, required=False,
+                                help="the FASTA reference that corresponds to BLASTX queries.")
+    parser_predict.set_defaults(func=predict_orf)
 
     ## Argument checking
     args = parser.parse_args()
-
-    print parse_blastx_args(args.blastx)
-
+    args.func(args)
+    
+    print args
+    # a = join_blastx_results(parse_blastx_args(args.blastx))
+    # cPickle.dump(a, file=open("joined_blastx_dbs.pkl", "wb"))
 
     # # first, we index the FASTA reference
     # ref_index = SeqIO.index(args.ref, "fasta")
     # ref_ids = ref_index.keys()
 
-    

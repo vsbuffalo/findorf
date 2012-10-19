b6e6f65f9178ec99335485bc228d5ea09b4dede3
Vince Buffalo <vsbuffalo@gmail.com>
2012-10-19 16:42:06 -0700

    added some tests

--- findorf/tests/test_orfprediction.py
+++ findorf/tests/test_orfprediction.py
@@ -9,12 +9,16 @@ from Bio import SeqIO
 from findorf.orfprediction import get_all_orfs
 
 def test_get_all_orfs_case_generator():
-    case_1 = dict(seq=SeqRecord(Seq("GGGGGGATGGGGGGGATGGGGATGGGGTAGGGGGGGATGGGG"), "case_1"),
-              frame=1, start=[0, 6, 15, 21, 36], end=[27, 27, 27, 27, 41])
+    cases = list()
+    cases.append(dict(seq=SeqRecord(Seq("GGGGGGATGGGGGGGATGGGGATGGGGTAGGGGGGGATGGGG"), "case with frame=1"),
+                      frame=1, start=[0, 6, 15, 21, 36], end=[27, 27, 27, 27, 41]))
 
-    case_2 = dict(seq=SeqRecord(Seq("CCCCATCCCCCCCTACCCCATCCCCATCCCCCCCATCCCCCCA"), "case_2"),
-                  frame=-2, start=[1, 7, 16, 22, 37], end=[28, 28, 28, 28, 42])
-    cases = [case_1, case_2]
+    cases.append(dict(seq=SeqRecord(Seq("CCCCATCCCCCCCTACCCCATCCCCATCCCCCCCATCCCCCCA"), "case with frame=-2"),
+                      frame=-2, start=[1, 7, 16, 22, 37], end=[28, 28, 28, 28, 42]))
+
+    # corner cases:
+    cases.append(dict(seq=SeqRecord(Seq("ATGTAG"), "case with frame=1, start stop"), frame=1,
+                      start=[0], stop=[4]))
     for case in cases:
         yield check_get_all_orfs, case['seq'], case['frame'], case['start'], case['end']
 
